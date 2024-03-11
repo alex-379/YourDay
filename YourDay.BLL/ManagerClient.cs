3f63d74c28ce8ddd6c5242e68a906d796b615e71
glamiturianmai <glamiturianmaioha@gmail.com>
2024-03-11 16:32:06 +0300

    first fix (not done)

---
+++ YourDay.BLL/ManagerClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourDay.BLL.Models.OutputModels;
+using YourDay.BLL.Models.InputModels;
+
+namespace YourDay.BLL
+{
+    public class ManagerClient
+    {
+        public List<OrderInputModel> orderInputModels;
+        public DateOnly date1;
+
+        public ManagerClient()
+        {
+            DateOnly date1 = new DateOnly(2015, 7, 20);
+            orderInputModels = new List<OrderInputModel>();
+            orderInputModels.Add(new OrderInputModel());
+            orderInputModels[0].Id = 1;
+            orderInputModels[0].OrderName = "День рождения";
+            orderInputModels[0].ClientName = "Аня";
+            orderInputModels[0].Adress = "Москва";
+            orderInputModels[0].Date = date1;
+            orderInputModels[0].CountPeople = 23;
+            orderInputModels[0].Comments = "много людей";
+
+            orderInputModels.Add(new OrderInputModel());
+            orderInputModels[1].Id = 2;
+            orderInputModels[1].OrderName = "Новый год";
+            orderInputModels[1].ClientName = "Михаил Сергеевич";
+            orderInputModels[1].Adress = "Новосибирск";
+            orderInputModels[0].Date = date1;
+            orderInputModels[1].CountPeople = 3;
+            orderInputModels[1].Comments = "мало людей";
+        }
+
+        public List<OrderInputModel> GetAllOrders()
+        {
+
+            var result = orderInputModels;
+
+            return result;
+        }
+
+        public void AddOrder(OrderInputModel order)
+        {
+            orderInputModels.Add((OrderInputModel)order);
+
+        }
+
+        public void AddOrderFields(string Name, DateOnly Date)
+        {
+            orderInputModels.Add(new OrderInputModel());
+            orderInputModels[3].OrderName = Name;
+            orderInputModels[3].Date = Date;
+        }
+    }
+}
---
+++ YourDay.BLL/Models/InputModels/OrderInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourDay.BLL.Models.InputModels
+{
+    public class OrderInputModel
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+        public string ClientName { get; set; }
+        public DateOnly Date { get; set; }
+        public int CountPeople { get; set; }
+        public string Adress { get; set; }
+        public string Comments { get; set; }
+
+
+    }
+}
