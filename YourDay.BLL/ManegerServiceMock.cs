7c78ea25d626db98c6623e84a12d809a57b6c413
Ustinova <rykoca.tanya.96@gmail.com>
2024-03-14 18:22:38 +0300

    create communication between the foreman and the manager

---
+++ YourDay.BLL/ManegerServiceMock.cs
@@ -0,0 +1,37 @@
+﻿
+using YourDay.BLL.IServices;
+using YourDay.BLL.Models.MenagerModels.OutputModel;
+
+namespace YourDay.BLL
+{
+    public class ManegerServiceMock : IManagerService
+    {
+        private List<MenagerNameAndPhoneOutputModel> _managers;
+
+        public ManegerServiceMock() 
+        {
+            _managers = new List<MenagerNameAndPhoneOutputModel>()
+            {
+                new MenagerNameAndPhoneOutputModel
+                {
+                    Name="Александр",
+                    Phone="2345"
+                },
+                new MenagerNameAndPhoneOutputModel
+                {
+                    Name="Екатерина",
+                    Phone="7806"
+                },
+                new MenagerNameAndPhoneOutputModel
+                {
+                    Name="Тимофей",
+                    Phone="454657"
+                }
+            };
+        }
+        public List<MenagerNameAndPhoneOutputModel> GetAllMenagers()
+        {
+            return _managers;
+        }
+    }
+}
