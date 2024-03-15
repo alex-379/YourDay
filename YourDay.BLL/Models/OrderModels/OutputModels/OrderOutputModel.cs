918426ad573eafcd1de93f430130f1ed42e3e8ba
glamiturianmai <glamiturianmaioha@gmail.com>
2024-03-15 19:40:44 +0300

    fix bll

--- YourDay.BLL/Models/OrderModels/OutputModels/OrderOutputModel.cs
+++ YourDay.BLL/Models/OrderModels/OutputModels/OrderOutputModel.cs
@@ -6,14 +6,24 @@
 
         public string OrderName { get; set; }
 
-        public string ClientName { get; set; }
+        public int ClientId { get; set; }
+
+        public int ManagerId { get; set; }
+
+        public int StatusId { get; set; }
 
         public string Date { get; set; }
 
-        public int CountPeople { get; set; }
-
         public string Adress { get; set; }
 
+        public int CountPeople { get; set; }
+
+        public int Price { get; set; }
+
+        public int Evaluation { get; set; }
+
         public string Comments { get; set; }
+
+        public bool IsDeleted { get; set; }
     }
 }
