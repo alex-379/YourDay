472eccdfb6276e56992094318b8b303afc4b2f3c
alex-379 <alexey_379@mail.ru>
2024-03-16 05:55:09 +1000

    add conString

---
+++ YourDay.DAL/Options.cs
@@ -0,0 +1,13 @@
+﻿namespace YourDay.DAL
+{
+    public class Options
+    {
+        public static string connectionString
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("YourDayDB");
+            }
+        }
+    }
+}
