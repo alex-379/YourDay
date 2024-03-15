409854ce9f0557b14dd519f32f4f2fe928c98ea1
alex-379 <alexey_379@mail.ru>
2024-03-16 00:12:57 +1000

    add UserModels

---
+++ YourDay.BLL/IServices/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using YourDay.BLL.Models.UserModels.InputModels;
+using YourDay.BLL.Models.UserModels.OutputModels;
+
+namespace YourDay.BLL.IServices
+{
+    public interface IUserService
+    {
+        public UserOutputModel AddUser(UserRegistrationInputModel user);
+    }
+}
---
+++ YourDay.BLL/UserServiceMock.cs
@@ -0,0 +1,20 @@
+﻿using YourDay.BLL.IServices;
+using YourDay.BLL.Models.UserModels.InputModels;
+using YourDay.BLL.Models.UserModels.OutputModels;
+
+namespace YourDay.BLL
+{
+    public class UserServiceMock : IUserService
+    {
+        public UserOutputModel AddUser(UserRegistrationInputModel user)
+        {
+            return new UserOutputModel
+            {
+                Id = 0,
+                UserName = user.UserName,
+                Mail = user.Mail,
+                Phone = user.Phone
+            };
+        }
+    }
+}
