6518bce76ea10f54d36e07e4f5e28882adc9122d
alex-379 <alexey_379@mail.ru>
2024-03-16 22:07:18 +1000

    add dtos

---
+++ YourDay.DAL/Dtos/HistoryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace YourDay.DAL.Dtos
+{
+    public class HistoryDto
+    {
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
--- YourDay.DAL/Dtos/UserDto.cs
+++ YourDay.DAL/Dtos/UserDto.cs
@@ -1,6 +1,27 @@
-﻿namespace YourDay.DAL.Dtos
+﻿using YourDay.DAL.Enums;
+
+namespace YourDay.DAL.Dtos
 {
     public class UserDto
     {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Password { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public List<OrderDto> ClientOrders { get; set; }
+
+        public List<OrderDto> ManagerOrders { get; set; }
+
+        public List<TaskDto> WorkerTasks { get; set; }
     }
 }
