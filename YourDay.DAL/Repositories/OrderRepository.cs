0c70ff9d6701b4fd0504ce780ce2c1f70c3a53f2
alex-379 <alexey_379@mail.ru>
2024-03-20 02:18:12 +1000

    upd repositories

--- YourDay.DAL/Repositories/OrderRepository.cs
+++ YourDay.DAL/Repositories/OrderRepository.cs
@@ -1,13 +1,36 @@
+using Microsoft.EntityFrameworkCore;
 using YourDay.DAL.Dtos;
+using YourDay.DAL.Enums;
 namespace YourDay.DAL.Repositories
 {
     public class OrderRepository
     {
-        public Context context;
-        public OrderRepository() {
+        readonly Context context = SingletoneStorage.GetStorage().Сontext;
 
-            Context context = SingletoneStorage.GetStorage().Сontext;
+        public IEnumerable<OrderDto> GetAllOrdersWithClientWithManagerWithStatus()
+        {
+            var orders = context.Orders
+                .Include(o=>o.Client)
+                .Include(o => o.Manager)
+                .ToList();
+            return orders;
         }
+
+        public OrderDto GetOrderByIdWithClientWithManagerWithStatus(int id)
+        {
+            var order = context.Orders
+                .Include(o => o.Client)
+                .Include(o => o.Manager)
+                .Where(o => o.Id == id)
+                .Single();
+            return order;
+        }
+
+        //public Context context;
+        //public OrderRepository() {
+
+        //    Context context = SingletoneStorage.GetStorage().Сontext;
+        //}
         public IEnumerable<OrderDto> GetAllOrders()
         {
             Context context = SingletoneStorage.GetStorage().Сontext;
