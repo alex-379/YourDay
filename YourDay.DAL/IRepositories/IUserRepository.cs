63a32d1e9ac853548dab5c6325716ca308b11152
alex-379 <alexey_379@mail.ru>
2024-03-19 23:04:01 +1000

    Auto stash before merge of "registration" and "origin/main"

---
+++ YourDay.DAL/IRepositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using YourDay.DAL.Dtos;
+
+namespace YourDay.DAL.IRepositories
+{
+    public interface IUserRepository
+    {
+        public int? AddUser(UserDto person);
+
+        public IEnumerable<UserDto> GetAllUsers();
+    }
+}
