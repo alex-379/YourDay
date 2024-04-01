1a18710b0c5535ad0281f4bd459821a6302662db
alex-379 <alexey_379@mail.ru>
2024-04-02 06:32:02 +1000

    fix workers

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using YourDay.DAL;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new();
+
+
+            var users =  context.Users
+                    .AsQueryable()
+                    .Include(c => c.Specializations)
+                    .Where(u => u.Role == YourDay.DAL.Enums.Role.Worker)
+                    .Where(u => u.IsDeleted == false).ToList();
+            Console.WriteLine();
+        }
+    }
+}
