83356d90c60922aa27ff30525d227a9c6fcf6c20
alex-379 <alexey_379@mail.ru>
2024-04-02 01:11:20 +1000

    mv create TablesForManagers at bll

---
+++ YourDay.BLL/TablesForUi/Column.cs
@@ -0,0 +1,8 @@
+﻿namespace YourDay.BLL.Tables
+{
+    public class Column
+    {
+        public string Field { get; set; }
+        public string LayoutHeader { get; set; }
+    }
+}
---
+++ YourDay.BLL/TablesForUi/Tables.cs
@@ -0,0 +1,30 @@
+﻿namespace YourDay.BLL.Tables
+{
+    public class Tables
+    {
+        public List<Column> Columns { get; set; }
+
+        public Tables()
+        {
+            Columns = new List<Column>();
+        }
+
+        public void GetTableForPrintManagers()
+        {
+            Columns.AddRange(
+                new Column[]
+                {
+                new Column()
+                {
+                    Field="Name",
+                    LayoutHeader="Имя"
+                },
+                new Column()
+                {
+                    Field="Phone",
+                    LayoutHeader="Телефон"
+                }
+        });
+        }
+    }
+}
