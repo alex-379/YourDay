0c2dfffa821660b99f6acc05396a596139ab0295
alex-379 <alexey_379@mail.ru>
2024-04-03 08:50:35 +1000

    fix nullable models in order

--- YourDay.BLL/Models/OrderModels/OutputModels/OrderOutputModel.cs
+++ YourDay.BLL/Models/OrderModels/OutputModels/OrderOutputModel.cs
@@ -9,7 +9,7 @@ namespace YourDay.BLL.Models.OrderModels.OutputModels
 
         public string? OrderName { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
         public DateTime? Date { get; set; }
 
